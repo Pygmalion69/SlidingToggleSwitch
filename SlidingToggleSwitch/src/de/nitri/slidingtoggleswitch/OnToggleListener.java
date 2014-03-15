33d68ec3c823b82df89929c5f50ddc5288d37f92
Pygmalion69 <pygmalion@xs4all.nl>
2014-03-15 19:28:30 +0100

    First version.

---
+++ SlidingToggleSwitch/src/de/nitri/slidingtoggleswitch/OnToggleListener.java
@@ -0,0 +1,6 @@
+package de.nitri.slidingtoggleswitch;
+
+public interface OnToggleListener {
+
+	public void onToggle(int result);
+}
---
+++ SlidingToggleSwitch/src/de/nitri/slidingtoggleswitch/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Pygmalion
+ *
+ */
+package de.nitri.slidingtoggleswitch;
